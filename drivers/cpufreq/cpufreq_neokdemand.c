8918aebd50e21affee8555efbf81b8d29d4cddaa
kgp700 <neokim20@gmail.com>
2012-11-15 00:54:52 +0900

    Neokdemand CPU Governor sysfs control fix

--- drivers/cpufreq/cpufreq_neokdemand.c
+++ drivers/cpufreq/cpufreq_neokdemand.c
@@ -352,7 +352,7 @@ static ssize_t store_debug_mask(struct kobject *kobj, struct attribute *attr, co
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0)
           debug_mask = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_up_rate_us(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -367,7 +367,7 @@ static ssize_t store_up_rate_us(struct kobject *kobj, struct attribute *attr, co
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0 && input <= 100000000)
           up_rate_us = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_down_rate_us(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -382,7 +382,7 @@ static ssize_t store_down_rate_us(struct kobject *kobj, struct attribute *attr, 
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0 && input <= 100000000)
           down_rate_us = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_up_min_freq(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -397,7 +397,7 @@ static ssize_t store_up_min_freq(struct kobject *kobj, struct attribute *attr, c
 	res = strict_strtoul(buf, 0, &input);
 	if (res >= 0 && input >= 0)
 	  up_min_freq = input;
-	return res;
+	return count;
 }
 
 static ssize_t show_sleep_max_freq(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -412,7 +412,7 @@ static ssize_t store_sleep_max_freq(struct kobject *kobj, struct attribute *attr
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0)
           sleep_max_freq = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_sleep_wakeup_freq(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -427,7 +427,7 @@ static ssize_t store_sleep_wakeup_freq(struct kobject *kobj, struct attribute *a
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0)
           sleep_wakeup_freq = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_awake_min_freq(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -442,7 +442,7 @@ static ssize_t store_awake_min_freq(struct kobject *kobj, struct attribute *attr
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0)
           awake_min_freq = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_sample_rate_jiffies(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -457,7 +457,7 @@ static ssize_t store_sample_rate_jiffies(struct kobject *kobj, struct attribute 
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input > 0 && input <= 1000)
           sample_rate_jiffies = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_ramp_up_step(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -472,7 +472,7 @@ static ssize_t store_ramp_up_step(struct kobject *kobj, struct attribute *attr, 
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0)
           ramp_up_step = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_ramp_down_step(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -487,7 +487,7 @@ static ssize_t store_ramp_down_step(struct kobject *kobj, struct attribute *attr
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input >= 0)
           ramp_down_step = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_max_cpu_load(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -502,7 +502,7 @@ static ssize_t store_max_cpu_load(struct kobject *kobj, struct attribute *attr, 
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input > 0 && input <= 100)
           max_cpu_load = input;
-        return res;
+        return count;
 }
 
 static ssize_t show_min_cpu_load(struct kobject *kobj, struct attribute *attr, char *buf)
@@ -517,7 +517,7 @@ static ssize_t store_min_cpu_load(struct kobject *kobj, struct attribute *attr, 
         res = strict_strtoul(buf, 0, &input);
         if (res >= 0 && input > 0 && input < 100)
           min_cpu_load = input;
-        return res;
+        return count;
 }
 
 #define define_global_rw_attr(_name)		\
